8b050bf8a81ad8b4b8f89f7f3f41c4666b5658eb
Matthias Oberholzer <matthias.m.oberholzer@gmail.com>
2021-11-19 17:00:22 +0100

    added WebGl Gradient

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,9 +1,29 @@
+import { useEffect } from 'react'
+import { Gradient } from 'utils/Gradient'
 import style from '../styles/hero.module.scss'
 
 export function Hero() {
+  useEffect(() => {
+    // Create your instance
+    const gradient = new Gradient(
+      document.querySelector('#gradient-canvas'),
+      window.innerWidth,
+      window.innerHeight
+    )
+
+    // Call `initGradient` with the selector to your canvas
+    // @ts-ignore
+    gradient.initGradient('#gradient-canvas')
+  }, [])
   return (
     <section className={style.hero}>
-      <div className={style.backgroundPlaceholder}>M</div>
+      {/* <div className={style.backgroundPlaceholder}>M</div> */}
+      <canvas
+        id="gradient-canvas"
+        className={style.backgroundPlaceholder}
+        data-transition-in
+      ></canvas>
+      <div className={style.backgroundGradient}></div>
       <div className={style.container}>
         <h1>
           I'm <i>baby microdosing flexitarian street</i> art pour-over
