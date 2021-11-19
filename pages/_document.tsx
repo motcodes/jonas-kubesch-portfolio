2cddcd0029b575cfc45ee6645df153520c4af957
Matthias Oberholzer <matthias.m.oberholzer@gmail.com>
2021-11-19 17:00:51 +0100

    Added font/color/sizes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,16 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang='en'>
-        <Head />
+      <Html lang="en">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          {/* @ts-ignore */}
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&family=Space+Grotesk:wght@600;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
