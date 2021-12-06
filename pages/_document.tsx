7301ca557b0a55ece1cfa23517f5a3b65fde0be2
Matthias Oberholzer <matthias.m.oberholzer@gmail.com>
2021-12-06 13:13:25 +0100

    turned off autotranslate

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html translate="no" lang="en">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           {/* @ts-ignore */}
